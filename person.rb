81b9b3d6e5e96adef92114fd20f575dd43053460
mandela.t <manderat206@gmail.com>
2022-07-20 00:44:28 +0200

    Add attribute reader for id

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 class Person
-  attr_accessor :age, :name, :id
+  attr_accessor :age, :name
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
