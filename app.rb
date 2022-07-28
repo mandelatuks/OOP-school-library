7bafad8fd34bffa249da66182ac531860b02ceff
Lembani Sakala <lembanisakala@gmail.com>
2022-07-28 16:20:17 +0200

    Extract methods from app: - Create rental - List all rentals - List all books - List all people

--- app.rb
+++ app.rb
@@ -40,26 +40,6 @@ class App
     sleep(0.5)
   end
 
-  def list_all_books
-    if @books.length.positive?
-      @books.each_with_index do |book, index|
-        puts "#{index + 1}) Tittle: #{book.title}, Author: #{book.author}"
-      end
-
-    else
-      message('there is no book to display', 'kindly add some books')
-    end
-  end
-
-  def list_all_people
-    if @people.length.positive?
-      @people.each_with_index do |person, index|
-        puts "#{index + 1}) Name:#{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-    else
-      message('There is no person to display', 'Add person profiles first')
-    end
-  end
 
   # def person_info
   #   print 'Age: '
@@ -89,34 +69,34 @@ class App
   #   message('Book created successfully', 'Happy learning')
   # end
 
-  def create_rental
-    puts 'Select a book from the following list by ID'
-    puts ''
-    list_all_books
-    selected_book = @books[gets.chomp.to_i - 1]
-    puts 'Select a person from the following list by serial number'
-    puts ''
-    list_all_people
-    selected_person = @people[gets.chomp.to_i - 1]
-    print 'Date: '
-    date = gets.chomp
-    @rentals << selected_person.add_rental(date, selected_book)
-    message('New rental created', "Date: #{date}, Rental to: #{selected_person.name}, Book: #{selected_book.title}")
-  end
+  # def create_rental
+  #   puts 'Select a book from the following list by ID'
+  #   puts ''
+  #   list_all_books
+  #   selected_book = @books[gets.chomp.to_i - 1]
+  #   puts 'Select a person from the following list by serial number'
+  #   puts ''
+  #   list_all_people
+  #   selected_person = @people[gets.chomp.to_i - 1]
+  #   print 'Date: '
+  #   date = gets.chomp
+  #   @rentals << selected_person.add_rental(date, selected_book)
+  #   message('New rental created', "Date: #{date}, Rental to: #{selected_person.name}, Book: #{selected_book.title}")
+  # end
 
-  def list_all_rentals
-    print 'Enter person ID: '
-    id = gets.chomp.to_i
-    person_details = @people.find { |person| person.id == id }
-    if person_details
-      puts 'Rentals'
-      person_details.rental.each_with_index do |rental, index|
-        puts "#{index + 1}) #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
-      end
-    else
-      message('there is no rental to display', 'please create rental record')
-    end
-  end
+  # def list_all_rentals
+  #   print 'Enter person ID: '
+  #   id = gets.chomp.to_i
+  #   person_details = @people.find { |person| person.id == id }
+  #   if person_details
+  #     puts 'Rentals'
+  #     person_details.rental.each_with_index do |rental, index|
+  #       puts "#{index + 1}) #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
+  #     end
+  #   else
+  #     message('there is no rental to display', 'please create rental record')
+  #   end
+  # end
 
   def start_app
     option = print_options
