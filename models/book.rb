a93101b0b7527acf7948c79b0fab2c40dd601662
Lembani Sakala <lembanisakala@gmail.com>
2022-07-30 10:04:07 +0200

    Update book

---
+++ models/book.rb
@@ -0,0 +1,16 @@
+require_relative './models/rental'
+
+class Book
+  attr_accessor :id, :title, :author, :rentals
+
+  def initialize(id, title, author)
+    @id = id || Random.rand(1...1000)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
+  end
+end
